12ccbbdfe59756f2361644ff8aea08e0a3fd04a2
Omar Azeez <omarazeezi14@gmail.com>
2025-06-27 21:21:12 +0530

    Delete week1 directory
    needed to follow guidelines

--- week1/FinancialForecasting/src/ForecastCalculator.java
+++
@@ -1,9 +0,0 @@
-public class ForecastCalculator {
-    // Recursive method to calculate future value
-    public static double calculateFutureValue(double presentValue, double rate, int years) {
-        if (years == 0) {
-            return presentValue;
-        }
-        return calculateFutureValue(presentValue * (1 + rate), rate, years - 1);
-    }
-}
